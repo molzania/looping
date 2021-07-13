74385cd5c37140a632cd16b98f3a9366cc11697c
Firsty Ukhti Molyndi <37396480+molzania@users.noreply.github.com>
2021-07-13 12:02:14 +0700

    Update soal5.js

--- soal5.js
+++ soal5.js
@@ -1,13 +1,8 @@
   
 let jawaban = prompt("Sebutkan kepanjangan dari nama IB ?");
 
-while(jawaban!=undefined){
-    prompt("Jawaban Kosong. Silahkan Ulangi!")
-if(jawaban!="Impact Byte"){
-jawaban = prompt("Sebutkan kepanjangan dari nama IB");
+while(true){
+   jawaban = prompt("Sebutkan kepanjangan dari nama IB?");
+if(jawaban=="Impact Byte") break;
 }
-else{
     alert("Selamat jawaban kamu benar. Kamu berhak dapat hadiah piring cantik.");
-    break;
-}
-}
